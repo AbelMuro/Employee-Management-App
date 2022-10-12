d3939af1d19c8e512b5a6452d82ee83e38550434
abelmuro <abelmuro93@gmail.com>
2022-10-12 13:53:24 -0700

    finally added a database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from 'firebase/database';
-import { getAuth, onAuthStateChanged} from 'firebase/auth';
+import { getDatabase, ref, set} from 'firebase/database';
+
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBxvSMl1zmBK4DeqgACqrPAD8tEGjh3b0U",
-  authDomain: "employee-management-app-ee938.firebaseapp.com",
-  projectId: "employee-management-app-ee938",
-  storageBucket: "employee-management-app-ee938.appspot.com",
-  messagingSenderId: "807475484166",
-  appId: "1:807475484166:web:39238dae11543a04f99587",
-  measurementId: "G-B89MCH70HV",
-  databaseURL: "https://employee-management-app-ee938-default-rtdb.firebaseio.com/"
+    apiKey: "AIzaSyBxvSMl1zmBK4DeqgACqrPAD8tEGjh3b0U",
+    authDomain: "employee-management-app-ee938.firebaseapp.com",
+    databaseURL: "https://employee-management-app-ee938-default-rtdb.firebaseio.com",
+    projectId: "employee-management-app-ee938",
+    storageBucket: "employee-management-app-ee938.appspot.com",
+    messagingSenderId: "807475484166",
+    appId: "1:807475484166:web:8f1f6620482b52ecf99587",
+    measurementId: "G-Y2VBPMPYRZ",
+    databaseURL: "https://employee-management-app-ee938-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
@@ -23,14 +24,13 @@ const app = initializeApp(firebaseConfig);
 // Initialize database
 const database = getDatabase(app);
 
-const auth = getAuth(app);
 
-// authenticate user 
-onAuthStateChanged(auth, (user) => {
-    if(user){
-        console.log("im logged in")
-    }
-    else{
-        console.log("im not logged in");
-    }
+
+//second argument is the name of the data that is stored in the database
+// '100' -> 'key' 'value'
+const reference = ref(database, '/');
+
+set(reference, {
+    example: "example"
 })
+
